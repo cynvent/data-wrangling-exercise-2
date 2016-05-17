a8204d8d8229f0fb71aad84dc69905a69d232515
cynvent <matt@cynvent.co.uk>
2016-05-17 14:58:08 -0700

    - mean Age added - alternatives to standard mean Age (mean Age by Gender) - Lifeboats update

--- data_wrangling_exercise_2.R
+++ data_wrangling_exercise_2.R
@@ -1,10 +1,11 @@
 #Set wd
 
+
 #Import the titanic_original dataset
 titanic_original <- read.csv("~/Desktop/Data Wrangling Exercises/data-wrangling-exercise-2/titanic_original.csv")
 
 #View the imported dataset
-view(titanic_original)
+View(titanic_original)
 
 #Port of Embarkation - Find the missing calues and replace them with "S"
 filter(titanic_original, embarked == "")
@@ -13,3 +14,16 @@ titanic_original$embarked[titanic_original$embarked == ""] <- "S"
 #Age - Calculate the mean of the Age column and use that to populate the missing values
 mean_age <- mean(titanic_original$age, trim = 0, na.rm = TRUE)
 titanic_original$age[is.na(titanic_original$age)] <- mean_age
+
+#Age - mean alternatives
+# Mean Age by Gender
+
+titanic_original %>% 
+  group_by(sex) %>% 
+  summarise(mean_age = mean(age, na.rm=FALSE))
+
+female_mean_age <- 28.88694
+male_mean_age <-30.43072
+
+#Lifeboats - replace blank values with NA
+titanic_original$boat[titanic_original$boat == ""] <- "NA"
